f780790f08c0d452df8bb592e3c867c392597c47
mayurCoder2004 <mayursomnathpawar123@gmail.con>
2025-06-02 09:49:16 +0530

    Updating AuthContext.jsx

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -15,7 +15,7 @@ export const AuthProvider = ({ children }) => {
     const [onlineUsers, setOnlineUsers] = useState([]);
     const [socket, setSocket] = useState(null);
 
-    // Check f user is authenticated and if so, set the user data and connect the socket
+    // Check if user is authenticated and if so, set the user data and connect the socket
 
     const checkAuth = async () => {
         try {
